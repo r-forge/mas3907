3bb5a0ae5c05fc843cb93130cea42f74e29cfbc8
sarahheaps (Dr ) <sarah.e.heaps@durham.ac.uk>
2022-10-04 18:17:54 +0000

    Hopefully corrected durham package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mas3907@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/durhamSLR/R/durhamSLR.R
@@ -0,0 +1,10 @@
+#' The durhamSLR package
+#'
+#' Data for Statistical Learning modules at Durham University.
+#'
+#' @docType package
+#'
+#' @author Sarah Heaps \email{sarah.e.heaps@durham.ac.uk}
+#'
+#' @name durhamSLR
+NULL
